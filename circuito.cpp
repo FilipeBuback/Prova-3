34f3d64e3afbb8612ca31a2f2fafdd8dee1743bf
FilipeBuback <56230660+FilipeBuback@users.noreply.github.com>
2021-06-20 16:03:09 +0000

    Versao 2

--- circuito.cpp
+++ circuito.cpp
@@ -1,24 +1,38 @@
 #include<iostream>
 
 using namespace std;
+namespace funcao{
 
-struct tCircuito{
 
-float R1;
-float R2;
-float V1;
-float serie;
-float corrente_serie;
-float potencia_serie;
-float paralelo;
-float corrente_paralelo;
-float potencia_paralelo;
+  struct tCircuito{
+    float r1;
+    float r2;
+    float v1;
+    float serie;
+    float corrente_serie;
+    float potencia_serie;
+    float paralelo;
+    float corrente_paralelo;
+    float potencia_paralelo;
+    void recebevalores(float R1,float R2, float V1){
+      r1=R1;
+      r2=R2;
+      v1=V1;
 
-serie=R1+R2;
-corrente_serie=V1/serie;
-potencia_serie=V1*corrente_serie;
-paralelo=((R1+R2)/(R1*R2));
-corrente_paralelo=V1/paralelo;
-potencia_paralelo=V1*corrente_paralelo
-
+    }
+    void calculo(){
+      serie=r1+r2;
+      corrente_serie=v1/serie;
+      potencia_serie=v1*corrente_serie;
+      paralelo=((r1+r2)/(r1*r2));
+      corrente_paralelo=v1/paralelo;
+      potencia_paralelo=v1*corrente_paralelo;
+      cout<< "Resultado da resistência em série" << serie << endl;
+      cout<< "Resultado da corrente em série" << corrente_serie << endl;
+      cout<< "Resultado da potência em série" << potencia_serie << endl;
+      cout<< "Resultado da resistência em paralelo" << paralelo << endl;
+      cout<< "Resultado da corrente em série" << corrente_paralelo << endl;
+      cout<< "Resultado da potência em série" << potencia_paralelo << endl;
+  }
+ }
 }
--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include "circuito.cpp"
-
+using namespace std;
+using namespace funcao;
 int main() {
-  std::cout << "Hello World!\n";
+  tCircuito circuito1;
+
+  circuito1.recebevalores(200,300,20);
+  circuito1.calculo();
 }
